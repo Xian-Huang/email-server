937dcdee4c211fb0b1793ddc0672ba73dbea5811
XianHuang <148949408+Xian-Huang@users.noreply.github.com>
2024-11-05 21:51:17 +0800

    添加第一个接口：health_check

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 
 async fn greet(req:HttpRequest)->impl Responder{
@@ -12,7 +12,15 @@ async fn main()->std::io::Result<()> {
         App::new()
         .route("/", web::get().to(greet))
         .route("/{name}", web::get().to(greet))
+        .route("/health_check", web::get().to(health_check))
+
     }).bind("127.0.0.1:8000")?
     .run()
     .await
+}
+
+
+// 健康检查器
+async fn health_check(_req:HttpRequest)->impl Responder{
+    HttpResponse::Ok().finish()
 }
