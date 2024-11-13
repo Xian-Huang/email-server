0fd74687146231cc6a8c98a4e57c9e90a9dc4619
XianHuang <148949408+Xian-Huang@users.noreply.github.com>
2024-11-13 23:03:34 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::net::TcpListener;
 use emailserver::run;
 
 
-
-
 #[tokio::main]
 async fn main()->std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address!");
