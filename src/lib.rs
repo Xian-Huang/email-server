ca2aad5f27f8a9f253883ce14e20692df092b22f
LiDingyi <10294099+li-tincture-instrument@user.noreply.gitee.com>
2025-03-18 14:36:07 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
 use std::net::TcpListener;
 pub mod configurations;
-use actix_web::{body::BoxBody, dev::Server, http::StatusCode, web::{self, Form}, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    body::BoxBody,
+    dev::Server,
+    http::StatusCode,
+    web::{self, Form},
+    App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use chrono::Utc;
 use sqlx::MySqlPool;
 
-
-
-#[derive(serde::Deserialize,Debug)]
-struct FormData{
-    name:String,
-    email:String
+#[derive(serde::Deserialize, Debug)]
+struct FormData {
+    name: String,
+    email: String,
 }
-
-
 
 // 健康检查器
 pub async fn health_check(_req: HttpRequest) -> HttpResponse {
@@ -24,28 +26,40 @@ async fn greet(req: HttpRequest) -> impl Responder {
     format!("Hello {name}!")
 }
 
-
-async fn subscribe(form:Form<FormData>,connection:web::Data<MySqlPool>)->HttpResponse{
-    let now =format!("{}", Utc::now().format("%Y-%m-%d %H:%M:%S"));
-    let result = sqlx::query!(r#"INSERT INTO subscriptions (email, name, subscribed_at) VALUES (?, ?, ?)"#,form.email,form.name,now).execute(connection.get_ref()).await;
+async fn subscribe(form: Form<FormData>, connection: web::Data<MySqlPool>) -> HttpResponse {
+    let now = format!("{}", Utc::now().format("%Y-%m-%d %H:%M:%S"));
+    let result = sqlx::query!(
+        r#"INSERT INTO subscriptions (email, name, subscribed_at) VALUES (?, ?, ?)"#,
+        form.email,
+        form.name,
+        now
+    )
+    .execute(connection.get_ref())
+    .await;
     match result {
         Ok(_) => {
             println!("success to insert");
-            HttpResponse::new(StatusCode::from_u16(200).unwrap()).set_body(BoxBody::new("success to insert"))
-        },
+            HttpResponse::new(StatusCode::from_u16(200).unwrap())
+                .set_body(BoxBody::new("Success to insert"))
+        }
         Err(e) => {
-            println!("Fail to insert:{}",e);
-            HttpResponse::new(StatusCode::from_u16(500).unwrap()).set_body(BoxBody::new("Fail to insert"))
-        },
+            println!("Fail to insert:{}", e);
+            if e.as_database_error().unwrap().is_unique_violation() {
+                HttpResponse::new(StatusCode::from_u16(200).unwrap())
+                    .set_body(BoxBody::new("Success to insert"))
+            } else {
+                HttpResponse::new(StatusCode::from_u16(500).unwrap())
+                    .set_body(BoxBody::new("Data exists,Fail to insert"))
+            }
+        }
     }
-    
 }
 
-pub fn run(listener:TcpListener,pool:MySqlPool) -> Result<Server, std::io::Error> {
-    let port= listener.local_addr().unwrap().port();
+pub fn run(listener: TcpListener, pool: MySqlPool) -> Result<Server, std::io::Error> {
+    let port = listener.local_addr().unwrap().port();
 
     let connection = web::Data::new(pool);
-    let server = HttpServer::new(move|| {
+    let server = HttpServer::new(move || {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
@@ -54,6 +68,6 @@ pub fn run(listener:TcpListener,pool:MySqlPool) -> Result<Server, std::io::Error
     })
     .listen(listener)?
     .run();
-    println!("RUN IN:127.0.0.1:{}",port);
+    println!("RUN IN:127.0.0.1:{}", port);
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,84 +3,88 @@ use std::net::TcpListener;
 use emailserver::configurations::get_config;
 use sqlx::{Connection, MySqlConnection, MySqlPool};
 
-
 #[tokio::test]
-async fn database_connect(){
+async fn database_connect() {
     let connect_url = get_config().unwrap().database.connext_string();
-    let mut connection = MySqlConnection::connect(&connect_url).await.expect("Faile to connect database");
+    let mut connection = MySqlConnection::connect(&connect_url)
+        .await
+        .expect("Faile to connect database");
     println!("connect to {connect_url} sccuess");
-    let saved = sqlx::query!("SELECT email,name FROM subscriptions",).fetch_one(&mut connection).await.expect("query error");
-    println!("{}-{}",saved.name,saved.email);
-    assert_eq!(saved.name,"test");
-    assert_eq!(saved.email,"test");
+    let saved = sqlx::query!("SELECT email,name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("query error");
+    println!("{}-{}", saved.name, saved.email);
+    assert_eq!(saved.name, "Li Dingyi");
+    assert_eq!(saved.email, "649295818@qq.com");
 }
 
-
-
-
-
 #[tokio::test]
-async fn health_check(){
-
+async fn health_check() {
     let address = spawn_app().await;
     let client = reqwest::Client::new();
-    let response = client.get(format!("{}/health_check",&address))
-    .send()
-    .await
-    .expect("Failed to send resquest!");
+    let response = client
+        .get(format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to send resquest!");
     assert!(response.status().is_success());
 }
 
-
-
 #[tokio::test]
-async fn subscribe_returns_400(){
-
+async fn subscribe_returns_400() {
     let address = spawn_app().await;
 
     let client = reqwest::Client::new();
 
     let test_cases = [
-        ("name=Li","missing the email"),
-        ("email=649295818%40qq.com","missing the name"),
-        ("","missing the name and email")
+        ("name=Li", "missing the email"),
+        ("email=649295818%40qq.com", "missing the name"),
+        ("", "missing the name and email"),
     ];
-    for (body,msg) in test_cases{
-        let response = client.post(format!("{}/subscribe",&address))
-        .body(body)
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .send()
-        .await
-        .expect("Failed to send resquest!");
-        assert_eq!(response.status().as_u16(), 400,"The API failed(400) for {}",msg);
+    for (body, msg) in test_cases {
+        let response = client
+            .post(format!("{}/subscribe", &address))
+            .body(body)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .send()
+            .await
+            .expect("Failed to send resquest!");
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API failed(400) for {}",
+            msg
+        );
     }
 }
 
 #[tokio::test]
-async fn subscribe_returns_200(){
-
+async fn subscribe_returns_200() {
     let address = spawn_app().await;
     let client = reqwest::Client::new();
 
     let body = "name=Li%20Dingyi&email=649295818%40qq.com";
 
-    let response = client.post(format!("{}/subscribe",&address))
-    .body(body)
-    .header("Content-Type", "application/x-www-form-urlencoded")
-    .send()
-    .await
-    .expect("Failed to send resquest!");
+    let response = client
+        .post(format!("{}/subscribe", &address))
+        .body(body)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .send()
+        .await
+        .expect("Failed to send resquest!");
     assert!(response.status().is_success());
 }
 
-
 //在后台启动应用程序
-async fn spawn_app()->String{
+async fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address!");
     let current_url = get_config().unwrap().database.connext_string();
     let port = listener.local_addr().unwrap().port();
-    let connection = MySqlPool::connect(&current_url).await.expect("fail to connect to database");
-    let server = emailserver::run(listener,connection).expect("Failed to start server");
+    let connection = MySqlPool::connect(&current_url)
+        .await
+        .expect("fail to connect to database");
+    let server = emailserver::run(listener, connection).expect("Failed to start server");
     tokio::spawn(server);
-    format!("http://127.0.0.1:{}",port)
+    format!("http://127.0.0.1:{}", port)
 }
