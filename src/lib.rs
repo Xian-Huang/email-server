0e51b7de92ff1ccb6805b9e51f84b635be3dbffc
LiDingyi <10294099+li-tincture-instrument@user.noreply.gitee.com>
2025-03-19 08:57:07 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
 use std::net::TcpListener;
 pub mod configurations;
-use actix_web::{body::BoxBody, dev::Server, http::StatusCode, web::{self, Form}, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    body::BoxBody,
+    dev::Server,
+    http::StatusCode,
+    web::{self, Form},
+    App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use chrono::Utc;
 use sqlx::MySqlPool;
 
-
-
-#[derive(serde::Deserialize,Debug)]
-struct FormData{
-    name:String,
-    email:String
+#[derive(serde::Deserialize, Debug)]
+struct FormData {
+    name: String,
+    email: String,
 }
-
-
 
 // 健康检查器
 pub async fn health_check(_req: HttpRequest) -> HttpResponse {
@@ -24,34 +26,44 @@ async fn greet(req: HttpRequest) -> impl Responder {
     format!("Hello {name}!")
 }
 
-
-async fn subscribe(form:Form<FormData>,connection:web::Data<MySqlPool>)->HttpResponse{
-    let now =format!("{}", Utc::now().format("%Y-%m-%d %H:%M:%S"));
+async fn subscribe(form: Form<FormData>, connection: web::Data<MySqlPool>) -> HttpResponse {
+    let now = format!("{}", Utc::now().format("%Y-%m-%d %H:%M:%S"));
     // 查询是否已经存在相同email的数据
-    let search_result = sqlx::query!(r#"SELECT id FROM subscriptions where email=?"#,form.email).fetch_one(connection.get_ref()).await;
-    if let Err(_) = search_result{
-        let result = sqlx::query!(r#"INSERT INTO subscriptions (email, name, subscribed_at) VALUES (?, ?, ?)"#,form.email,form.name,now).execute(connection.get_ref()).await;
+    let search_result = sqlx::query!(r#"SELECT id FROM subscriptions where email=?"#, form.email)
+        .fetch_one(connection.get_ref())
+        .await;
+    if let Err(_) = search_result {
+        let result = sqlx::query!(
+            r#"INSERT INTO subscriptions (email, name, subscribed_at) VALUES (?, ?, ?)"#,
+            form.email,
+            form.name,
+            now
+        )
+        .execute(connection.get_ref())
+        .await;
         match result {
             Ok(_) => {
                 println!("success to insert");
-                HttpResponse::new(StatusCode::from_u16(200).unwrap()).set_body(BoxBody::new("success to insert"))
-            },
+                HttpResponse::new(StatusCode::from_u16(200).unwrap())
+                    .set_body(BoxBody::new("success to insert"))
+            }
             Err(e) => {
-                println!("Fail to insert:{}",e);
-                HttpResponse::new(StatusCode::from_u16(500).unwrap()).set_body(BoxBody::new("Fail to insert"))
-            },
+                println!("Fail to insert:{}", e);
+                HttpResponse::new(StatusCode::from_u16(500).unwrap())
+                    .set_body(BoxBody::new("Fail to insert"))
+            }
         }
-    }else{
-        HttpResponse::new(StatusCode::from_u16(500).unwrap()).set_body(BoxBody::new("Data already exists"))
+    } else {
+        HttpResponse::new(StatusCode::from_u16(500).unwrap())
+            .set_body(BoxBody::new("Data already exists"))
     }
-
 }
 
-pub fn run(listener:TcpListener,pool:MySqlPool) -> Result<Server, std::io::Error> {
-    let port= listener.local_addr().unwrap().port();
+pub fn run(listener: TcpListener, pool: MySqlPool) -> Result<Server, std::io::Error> {
+    let port = listener.local_addr().unwrap().port();
 
     let connection = web::Data::new(pool);
-    let server = HttpServer::new(move|| {
+    let server = HttpServer::new(move || {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
@@ -60,6 +72,6 @@ pub fn run(listener:TcpListener,pool:MySqlPool) -> Result<Server, std::io::Error
     })
     .listen(listener)?
     .run();
-    println!("RUN IN:127.0.0.1:{}",port);
+    println!("RUN IN:127.0.0.1:{}", port);
     Ok(server)
 }
