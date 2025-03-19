d2f3a67918f9b88c4f392ac18021a1cb50ad661a
XianHuang <148949408+Xian-Huang@users.noreply.github.com>
2025-03-19 23:47:16 +0800

    测试隔离，添加测试数据库

--- src/configurations.rs
+++ src/configurations.rs
@@ -1,31 +1,43 @@
 use config::ConfigError;
 use serde;
 
-
-
 #[derive(serde::Deserialize)]
-pub struct Settings{
-    pub database:DatabaseSettings,
-    pub application_port:u16
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
 }
 
 #[derive(serde::Deserialize)]
-pub struct DatabaseSettings{
-    pub username:String,
-    pub password:String,
-    pub port:u16,
-    pub host:String,
-    pub dbname:String
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub dbname: String,
 }
 
-
-pub fn get_config()->Result<Settings,ConfigError>{
-    let settings = config::Config::builder().add_source(config::File::new("configurations.yaml",config::FileFormat::Yaml)).build()?;
+pub fn get_config() -> Result<Settings, ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configurations.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
     settings.try_deserialize::<Settings>()
 }
 
 impl DatabaseSettings {
-    pub fn connext_string(&self)->String{
-        format!("mysql://{}:{}@{}:{}/{}",self.username,self.password,self.host,self.port,self.dbname)
+    pub fn connection_string(&self) -> String {
+        format!(
+            "mysql://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.dbname
+        )
+    }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "mysql://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use std::net::TcpListener;
 use emailserver::{configurations::get_config, run};
 use sqlx::MySqlPool;
 
-
 #[tokio::main]
-async fn main()->std::io::Result<()> {
+async fn main() -> std::io::Result<()> {
     let settings = get_config().unwrap();
-    let connect_url = settings.database.connext_string();
+    let connect_url = settings.database.connection_string();
     let dbconpool = MySqlPool::connect(&connect_url).await.unwrap();
 
-    let address = format!("127.0.0.1:{}",settings.application_port);
+    let address = format!("127.0.0.1:{}", settings.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind address!");
-    run(listener,dbconpool)?.await
+    run(listener, dbconpool)?.await
 }
